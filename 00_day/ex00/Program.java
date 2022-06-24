7cccb48f7cf725d81e6ea07b0c14cb10e4108679
lavrenovamaria <wrickard@student.21-school.ru>
2022-06-24 19:15:46 +0300

    init

---
+++ 00_day/ex00/Program.java
@@ -0,0 +1,11 @@
+class Program {
+    public static void main(String[] args) {
+        int a = 479598;
+        int sum = 0;
+        while (a > 0) {
+            sum += a % 10;
+            a /= 10;
+        }
+        System.out.println(sum);
+    }
+}
