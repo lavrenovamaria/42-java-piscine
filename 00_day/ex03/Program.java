9954416e92a102ef67a345c901e61b6124775292
lavrenovamaria <wrickard@student.21-school.ru>
2022-06-28 19:08:20 +0300

    init

--- 00_day/ex03/Program.java
+++ 00_day/ex03/Program.java
@@ -11,8 +11,34 @@ public class Program {
                 System.out.println(str);
                 return;
             }
-            str.append(strOfWeek).append(" ").append('\n');
+            //дописать проверку на номер недели
+            str.append(strOfWeek).append(" ");
+
+           	System.out.print("-> ");
+           	Scanner values = new Scanner(System.in);
+           	str.append(getMin(values));
         }
         System.out.println(str);
+    }
+
+    static String getMin(Scanner values){
+        StringBuilder result = new StringBuilder();
+        //дописать проверку на количество значений
+        //проверку на величину каждого значения не более 9
+        values.useDelimiter(" ");
+    	int min;// = values.nextInt();
+    	//System.out.println(min);
+    	while(true){
+// 			int num = values.nextInt();
+// 			if(num < min)
+// 				min = num;
+		System.out.println(values.nextInt());
+		if(!values.hasNext())
+			break;
+		}
+// 		for(int i = 0; i < min; ++i)
+//             result.append("=");
+        result.append(">\n");
+		return result.toString();
     }
 }
