4eba0009ee6abfda191a71ad7b56aa8a2e4fb8dd
lavrenovamaria <wrickard@student.21-school.ru>
2022-06-28 18:08:52 +0300

    init

--- 00_day/ex03/Program.java
+++ 00_day/ex03/Program.java
@@ -1,19 +1,18 @@
 import java.util.Scanner;
 
 public class Program {
-	public static void main(String[] args) {
-		String str = "";
-		for (int i = 1; i <= 18; ++i) {
-			System.out.print("-> ");
+    public static void main(String[] args) {
+        StringBuilder str = new StringBuilder();
+        for (int i = 1; i <= 18; ++i) {
+            System.out.print("-> ");
             Scanner week = new Scanner(System.in);
             String strOfWeek = week.nextLine();
             if (strOfWeek.equals("42")) {
                 System.out.println(str);
-				return;
-			}
-			str += strOfWeek + " ";
-			str += '\n';
-		}
-		System.out.println(str);
-	}
+                return;
+            }
+            str.append(strOfWeek).append(" ").append('\n');
+        }
+        System.out.println(str);
+    }
 }
